a95d3300f26e9b0a48571715a4b25a79ab585011
Angel Araya <al.arayaq@gmail.com>
2017-10-19 23:03:03 -0600

    Add jwt auth barebones

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,60 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken')
 
-router.get('/hello', function(req, res) {
-  res.send('Hello, world!')
+router.post('/auth', function (req, res) {
+  if (req.body.user === 'angel' && req.body.password === 'damian') {
+    const payload = {
+      authenticated: true
+    }
+
+    const token = jwt.sign(payload, 'secret', {
+      expiresIn: '1 day'
+    })
+
+    res.json({
+      success: true,
+      message: 'Authenticated',
+      token: token
+    })
+  }
+  else {
+    res.status(401).json({
+      success: false,
+      message: 'Wrong user/password'
+    })
+  }
+})
+
+router.use(function (req, res, next) {
+  const token = req.body.token || req.query.token || req.headers['x-auth-token']
+
+  if (!token) {
+    res.status(403).json({
+      success: false,
+      message: 'Not authenticated'
+    })
+  } else {
+    jwt.verify(token, 'secret', function (error, decoded) {
+      if (error) {
+        res.status(403).json({
+          success: false,
+          message: "Not authenticated"
+        })
+      } else {
+        req.decoded = decoded
+        next()
+      }
+    })
+  }
+})
+
+router.post('/new', function(req, res) {
+  console.log(req.body.text)
+  res.json({
+    success: true,
+    message: 'Posted'
+  })
 })
 
 module.exports = router;
